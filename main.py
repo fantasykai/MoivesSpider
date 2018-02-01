c91c9347442232be2fbc687747c185da3622c706
makai <kai.fantasy@gmail.com>
2018-02-01 19:06:30 +0800

    数据库更新为mongo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 
 
 import sqlite3
@@ -9,6 +9,8 @@ from model.TaskQueue import TaskQueue
 from thread.FloorWorkThread import FloorWorkThread
 from thread.TopWorkThread import TopWorkThread
 from utils.Utils import Utils
+from pymongo import MongoClient, uri_parser
+from secret import mongo
 
 '''
     程序主入口
@@ -17,10 +19,16 @@ from utils.Utils import Utils
 '''
 
 # 截止到2017-08-08, 最新电影一共才有 164 个页面
-LASTEST_MOIVE_TOTAL_SUM = 6 #164
+LASTEST_MOIVE_TOTAL_SUM = 6  # 164
 
 # 请求网络线程总数, 线程不要调太好, 不然会返回很多 400
 THREAD_SUM = 5
+
+
+def getmongodb():
+    client = MongoClient(mongo['url'].split('?')[0])
+    parsed = uri_parser.parse_uri(mongo['url'])
+    return client[parsed['database']]
 
 
 def startSpider():
@@ -52,7 +60,6 @@ def startSpider():
         workthread = TopWorkThread(TaskQueue.getMiddleQueue(), i)
         workthread.start()
 
-
     while True:
         if TaskQueue.isMiddleQueueEmpty():
             break
@@ -63,63 +70,15 @@ def startSpider():
 
 
 def insertData():
-    DBName = 'dytt.db'
-    db = sqlite3.connect('./' + DBName, 10)
-    conn = db.cursor()
-
-    SelectSql = 'Select * from sqlite_master where type = "table" and name="lastest_moive";'
-    CreateTableSql = '''
-        Create Table lastest_moive (
-            'm_id' INTEGER PRIMARY KEY,
-            'm_type' varchar(100),
-            'm_trans_name' varchar(200),
-            'm_name' varchar(100),
-            'm_decade' varchar(30),
-            'm_conutry' varchar(30),
-            'm_level' varchar(100),
-            'm_language' varchar(30),
-            'm_subtitles' varchar(100),
-            'm_publish' varchar(30),
-            'm_IMDB_socre' varchar(50),
-            'm_douban_score' varchar(50),
-            'm_format' varchar(20),
-            'm_resolution' varchar(20),
-            'm_size' varchar(10),
-            'm_duration' varchar(10),
-            'm_director' varchar(50),
-            'm_actors' varchar(1000),
-            'm_placard' varchar(200),
-            'm_screenshot' varchar(200),
-            'm_ftpurl' varchar(200),
-            'm_dytt8_url' varchar(200)
-        );
-    '''
-
-    InsertSql = '''
-        Insert into lastest_moive(m_type, m_trans_name, m_name, m_decade, m_conutry, m_level, m_language, m_subtitles, m_publish, m_IMDB_socre, 
-        m_douban_score, m_format, m_resolution, m_size, m_duration, m_director, m_actors, m_placard, m_screenshot, m_ftpurl,
-        m_dytt8_url)
-        values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-    '''
-
-    if not conn.execute(SelectSql).fetchone():
-        conn.execute(CreateTableSql)
-        db.commit()
-        print('====  创建表成功  ====')
-    else:
-        print('====  创建表失败, 表已经存在  ====')
+    db = getmongodb()
 
     count = 1
 
     while not TaskQueue.isContentQueueEmpty():
         item = TaskQueue.getContentQueue().get()
-        conn.execute(InsertSql, Utils.dirToList(item))
-        db.commit()
+        db.lastest_moive.insert_one(item)
         print('插入第 ' + str(count) + ' 条数据成功')
         count = count + 1
-
-    db.commit()
-    db.close()
 
 
 if __name__ == '__main__':
---
+++ secret.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+mongo = {'url': 'mongodb://127.0.0.1:27017/bludb?replicaSet=mgset-1059591'}
